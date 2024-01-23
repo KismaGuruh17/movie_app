58e958db89ce9add2465ea20b43a329ab5400f99
titiknk <titikhofifa@gmail.com>
2024-01-23 22:31:24 +0700

    update MovieScreen (item detail)

--- src/Screens/MovieScreen.js
+++ src/Screens/MovieScreen.js
@@ -1,157 +1,70 @@
-import React, { useEffect, useState } from 'react';
-import { StyleSheet, Text, View, Image, ScrollView, TouchableOpacity } from 'react-native';
-import { Ionicons } from '@expo/vector-icons';
-import { StatusBar } from 'expo-status-bar';
-import Colors from '../constans/Colors';
-
-const MovieScreen = ({ route, navigation }) => {
+import React, { useState, useEffect } from 'react';
+import { View, Text, Image, StyleSheet, ScrollView } from 'react-native';
+ 
+const MovieScreen = ({ route }) => {
   const { movieId } = route.params;
   const [movieData, setMovieData] = useState(null);
-  const [castData, setCastData] = useState([]);
-
+ 
   useEffect(() => {
-    const fetchMovieData = async () => {
+    const fetchData = async () => {
       try {
-        const apiKey = '61b93257091c63f99ac3b8eca0c97863';
-
-        // Fetch movie details
-        const movieResponse = await fetch(
-          `https://api.themoviedb.org/3/movie/${movieId}?api_key=${apiKey}&language=en-US`
-        );
-        const movieData = await movieResponse.json();
-        setMovieData(movieData);
-
-        // Fetch cast details
-        const castResponse = await fetch(
-          `https://api.themoviedb.org/3/movie/${movieId}/credits?api_key=${apiKey}`
-        );
-        const castData = await castResponse.json();
-        setCastData(castData.cast);
+        const response = await fetch(`https://api.themoviedb.org/3/movie/${movieId}?api_key=61b93257091c63f99ac3b8eca0c97863`);
+        const json = await response.json();
+        setMovieData(json);
       } catch (error) {
-        console.error('Error fetching movie data:', error);
+        console.error(error);
       }
     };
-
-    fetchMovieData();
+ 
+    fetchData();
   }, [movieId]);
-
-  if (!movieData || castData.length === 0) {
+ 
+  if (!movieData) {
     return (
-      <View style={styles.container}>
+      <View style={styles.loadingContainer}>
         <Text>Loading...</Text>
-        <StatusBar style="auto" />
       </View>
     );
   }
-
+ 
   return (
-    <View style={styles.container}>
-      <TouchableOpacity style={styles.tombolKembali} onPress={() => navigation.goBack()}>
-        <Ionicons name="ios-arrow-back" size={30} color="#FFFFFF" />
-      </TouchableOpacity>
-
-      <ScrollView style={styles.scrollContainer}>
-        <Image
-          source={{
-            uri: `https://image.tmdb.org/t/p/w500${movieData.poster_path}`,
-          }}
-          style={styles.poster}
-        />
-
-        <Text style={styles.title}>{movieData.title}</Text>
-        <Text style={styles.description}>{movieData.overview}</Text>
-
-        {/* <View style={styles.overviewContainer}>
-          <Text style={styles.overviewTitle}>overview</Text>
-          <Text style={styles.overviewText}>{movie?.overview}</Text>
-        </View>   */}
-        <Text style={styles.title}>Cast:</Text>
-        <ScrollView horizontal showsHorizontalScrollIndicator={false}>
-          {castData.map((cast) => (
-            // menambah pengecekan untuk menampilkan gambar cast yang memiliki path
-            cast.profile_path && (
-              <View key={cast.id} style={styles.castContainer}>
-                <Image
-                  source={{
-                    uri: `https://image.tmdb.org/t/p/w200${cast.profile_path}`,
-                  }}
-                  style={styles.castImage}
-                />
-                <Text style={styles.castName}>{cast.name}</Text>
-              </View>
-            )
-          ))}
-        </ScrollView>
-      </ScrollView>
-
-      <StatusBar style="auto" />
-    </View>
+    <ScrollView style={styles.container}>
+      <Image
+        source={{ uri: `https://image.tmdb.org/t/p/w500${movieData.poster_path}` }}
+        style={styles.posterImage}
+      />
+    </ScrollView>
   );
 };
-
+ 
 const styles = StyleSheet.create({
   container: {
+    backgroundColor: 'black',
+  },
+  loadingContainer: {
     flex: 1,
-    backgroundColor: Colors.BLACK,
-    padding: 16,
-  },
-  tombolKembali: {
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    zIndex: 1,
-    padding: 5,
-    borderRadius: 10,
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
   },
-  poster: {
+  posterImage: {
     width: '100%',
     height: 300,
     resizeMode: 'cover',
-    borderRadius: 16,
-    marginBottom: 16,
   },
-  scrollContainer: {
-    flex: 1,
-    paddingHorizontal: 20,
+  contentContainer: {
+    padding: 16,
   },
   title: {
     fontSize: 24,
     fontWeight: 'bold',
+    color: 'white',
     marginBottom: 8,
-    textAlign: 'center',
-    color: Colors.WHITE,
   },
-  description: {
+  subtitle: {
     fontSize: 16,
-    textAlign: 'center',
-    marginBottom: 16,
-    color: Colors.WHITE,
+    color: 'white',
+    marginBottom: 4,
   },
-  castContainer: {
-    marginRight: 10,
-    alignItems: 'center',
-  },
-  castImage: {
-    width: 100,
-    height: 150,
-    borderRadius: 8,
-    marginBottom: 8,
-  },
-  castName: {
-    textAlign: 'center',
-    color: Colors.WHITE,
-  },
-  // overviewContainer: {
-  //   backgroundColor: Colors.EXTRA_LIGHT_GRAY,
-  // },
-  // overviewText: {
-
-  // },
-  // overviewText: {
-
-  // },
 });
-
+ 
 export default MovieScreen;
